021d1ea38b6340296e8427b6845b47ce8377ac36
roxannethomas <roxthomas@auchan.fr>
2019-04-25 09:26:19 +0200

    Merge Roxanne

--- main.js
+++ main.js
@@ -9,11 +9,17 @@ const Window = require('./lib/Window');
 
 // Primary function
 function main() {
-    let windows = new Window({file: path.join('public/views', 'index.html'), openDevTools: false});
+    let windows = new Window({file: path.join('public/views', 'index.html'), openDevTools: true, height: 550});
 
     ipcMain.on('calcul', (evt, data) => {
-    
+        let graph = new Window({file: path.join('public/views', 'echarts.html'), openDevTools: false});
+        graph.once('show', (evt2, data2) => {
+            graph.webContents.send('calcul', data);
+        })
+        windows.close();
+        windows = null;
     });
+
 }
 
 // When Electron is ready, launch the function
@@ -23,3 +29,20 @@ app.on('ready', main);
 app.on('window-all-closed', () => {
     app.quit();
 });
+
+var option = {
+    xAxis: {
+        type: 'value',
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [[120, 120], [130, 130], [140, 140]],
+        type: 'line'
+    },
+    {
+        data: [[90, 90], [80, 70], [70, 50]],
+        type: 'line'
+    }]
+};
---
+++ public/js/echarts.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const { ipcRenderer } = require('electron');
+var echarts = require('echarts');
+require('../../lib/prototype');
+
+ipcRenderer.on('calcul', (event, data) => {
+  // define max length
+  let max = data.val.length;
+
+  // based on prepared DOM, initialize echarts instance
+  var myChart = echarts.init(document.getElementById('main'));
+
+  // specify chart configuration item and data
+  var option = {
+    title: {
+      text: 'Représentation graphique'
+    },
+    xAxis: {
+      type: 'value',
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: []
+ };
+     
+  for(let i = 0; i < max; i++) {
+    let results = {
+      data : [],
+      type: 'line'
+    };
+    let firstDot = [0,0];
+    let secondDot = [0,0];
+    firstDot[0] = data.val[i] / data.x1[i];
+    secondDot[1] = data.val[i] / data.x2[i];
+    results.data.push(firstDot);
+    results.data.push(secondDot);
+    option.series.push(results);
+    
+  }
+  console.log('option ', option);
+  // use configuration item and data specified to show chart
+  myChart.setOption(option);
+});
+
